a1e6ba8ea0c84e0de654839a807944036ecd80f1
ikh05 <mi3061923002@gmail.com>
2025-09-13 19:55:52 +0800

    tes

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@
 fetch(`https://github-proxy-netlify.netlify.app/.netlify/functions/github`)
   .then(res => res.json())
   .then(data => {
+    console.log(data);
     document.getElementById("avatar").src = data.avatar_url;
     document.getElementById("name").textContent = data.name || data.login;
     document.getElementById("bio").textContent = data.bio || "Tidak ada bio.";

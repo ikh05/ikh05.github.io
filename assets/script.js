381e1b292d76d342a531f5bb0d9808a2a9502627
ikh05 <mi3061923002@gmail.com>
2025-09-14 19:18:29 +0800

    sds

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,4 @@ fetchGitHub("user", function(repos){
   document.getElementById("name").textContent = repos.name || repos.login;
   document.getElementById("bio").textContent = repos.bio || "Tidak ada bio.";
   document.getElementById("githubLink").href = repos.html_url;
-  document.getElementById("footerGithubLink").href = repos.html_url;
-  document.getElementById("footerGithubLink").innerHTML = '<i class="fa-brands fa-github"></i>' . repo.name;
 }); 

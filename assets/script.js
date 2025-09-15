f3d3a8330a54ef967ec4c8dc8d9adf0232907812
ikh05 <mi3061923002@gmail.com>
2025-09-16 06:51:59 +0800

    coba push

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-async function fetchGitHub(type) {
+async function fetchGitHub(type, body = {}) {
   try {
     const res = await fetch("https://github-token-ikh05.netlify.app/.netlify/functions/github", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ type }) // "user" atau "repos"
+      body: JSON.stringify({ type, ...body }) // type => "user", "repos", "file"
     });
 
     if (!res.ok) {
@@ -31,6 +31,20 @@ async function fetchGitHub(type) {
   document.getElementById("name").textContent = user.name || user.login;
   document.getElementById("bio").textContent = user.bio || "Tidak ada bio.";
   document.getElementById("githubLink").href = user.html_url;
+  document.getElementById("githubLink").innerHTML = `<i class="fa-brands fa-github"></i> ${user.login}`;
   document.getElementById("footerGithubLink").href = user.html_url;
   document.getElementById("footerGithubLink").innerHTML = `<i class="fa-brands fa-github"></i> ${user.name || user.login}`;
+})();
+
+
+(async ()=> {
+  const file = await fetchGitHub("file", { 
+    path: "readme.md", 
+    repo: "ikh05" 
+  });
+  if (file.error) {
+    document.getElementById("Readme.md").textContent = "Gagal ambil data.";
+    return;
+  }
+  console.log(file);
 })();

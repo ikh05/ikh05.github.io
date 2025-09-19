e13c764b7186a26b259c4ca3c06907d48b68916a
ikh05 <mi3061923002@gmail.com>
2025-09-20 07:27:28 +0800

    asas

--- assets/core.js
+++ assets/core.js
@@ -1,8 +1,4 @@
-import AOS from "https://unpkg.com/aos@2.3.1/dist/aos.js";
 import getContrastColor from './luminance.js';
-
-
-AOS.init();
 
 console.log(getContrastColor("#ffffff")); // Output: "black"
 console.log(getContrastColor("rgb(0, 0, 0)")); // Output: "light"
